fe8bb32e02e0d534b795dda3a58fada5153a7d36
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-07-18 11:15:15 +0100

    ..

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+function App() {
+  return (
+    <div className="bg-blue-500 p-4">
+      <h1 className="text-white bg-black text-center text-2xl">Hello Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
